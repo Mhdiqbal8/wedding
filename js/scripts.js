7658a6a9abc73cbded70c10eef02e6560f4a79de
mhdiqbal8 <muh.iqbal976@gmail.com>
2026-05-02 16:32:03 +0700

    update scroll navigation

--- js/scripts.js
+++ js/scripts.js
@@ -111,3 +111,12 @@ document.addEventListener("DOMContentLoaded", function () {
     nextBtn.click();
   }, 5000);
 });
+
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("navbar-scrolled");
+  } else {
+    navbar.classList.remove("navbar-scrolled");
+  }
+});
